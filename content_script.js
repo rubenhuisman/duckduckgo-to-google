fb93c6524b933cfdf4912afbefdabff5eb980886
Ruben Huisman <mail@rubenhuisman.nl>
2015-09-11 20:41:14 +0200

    Removed useless console.log()

--- content_script.js
+++ content_script.js
@@ -2,7 +2,6 @@ function myfunc(){
     var searchText = $("input[name=q]").val();
     $(".zcm").append('<li class="zcm__item"><a id="customAddedItem" class="zcm__link  js-zci-link  js-zci-link--images  "  target="_blank" href="#">Do a Google Search</a></li>');
     $("#customAddedItem").attr("href", "https://www.google.nl/?gfe_rd=cr&ei=0xfzVf3vO4am8wemx7qICg#q=" + searchText);
-    console.log($(location).attr('href'));
 }
 
 $(document).ready(myfunc);
