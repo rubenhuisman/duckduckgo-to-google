e74ce840062fc4b8657dc567cc1a0821b4d1458a
Ruben Huisman <mail@rubenhuisman.nl>
2017-03-15 08:32:04 +0000

    Fixed urlencoding

--- content_script.js
+++ content_script.js
@@ -1,6 +1,7 @@
 function addGoogleSearchToDom(){
     var searchText = $("input[name=q]").val();
     $(".zcm").append('<li class="zcm__item"><a id="customAddedItem" class="zcm__link  js-zci-link  js-zci-link--images  "  target="_blank" href="#">Do a Google Search</a></li>');
+    searchText = encodeURIComponent(searchText);
     $("#customAddedItem").attr("href", "https://encrypted.google.com/#q=" + searchText);
 }
 
